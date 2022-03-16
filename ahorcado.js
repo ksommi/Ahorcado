115ae8dec64db87de47782fedaa56b2a97e3470f
ksommi <ksommi@gmail.com>
2022-03-16 14:36:37 -0300

    v0.0.2

--- ahorcado.js
+++ ahorcado.js
@@ -14,6 +14,7 @@ var addWordBtn = document.querySelector("#enviar-palabra");
 var wordForm = document.querySelector("#form-palabra");
 var wordInput = document.querySelector("#input-palabra");
 var wordGame = document.querySelector("#palabra-juego");
+var keyGame = document.querySelector(".letras-tecleadas");
 
 /* Eventos */
 showFormBtn.addEventListener("click", showInput);
@@ -22,12 +23,15 @@ backBtn.addEventListener("click", goHome);
 addWordBtn.addEventListener("click", sendWord);
 
 /* Juego */
-var wordsSaved = ["4444", "55555", "666666", "1", "22", "333", "7777777"];
+var wordsSaved = ["tres", "cinco", "cuatro", "y", "se", "ser", "factura"];
 var wordsCount = wordsSaved.length - 1;
 
-/* Variables */
+/* Declaraciones */
 var wordArray = [];
 var stringWord = "";
+var arrayUnderscore = [];
+var keyPressed = [];
+var tecla = "";
 
 /* Funciones */
 
@@ -42,8 +46,8 @@ function generateWord() {
   return wordArray;
 }
 
-function showWord() {
-  wordGame.textContent = stringWord;
+function showWord(word) {
+  wordGame.textContent = word;
 }
 
 function showInput() {
@@ -55,12 +59,14 @@ function goGame() {
   homeScreen.style.display = "none";
   generateWord();
   hideWord(wordArray);
-  showWord();
+  showWord(stringWord);
+  listenKey();
 }
 
 function goHome() {
   gameScreen.style.display = "none";
   homeScreen.style.display = "block";
+  location.reload();
 }
 
 function sendWord() {
@@ -70,10 +76,53 @@ function sendWord() {
 }
 
 function hideWord(array) {
-  var arrayUnderscore = [];
   for (var i = 0; i < array.length; i++) {
     arrayUnderscore.push("_");
   }
   stringWord = arrayUnderscore.join("");
   return stringWord;
+}
+
+function validateKey() {
+  var valids = /[a-zA-Z]/.test(tecla);
+  return valids;
+}
+
+function listenKey(e) {
+  window.addEventListener("keypress", (e) => {
+    tecla = e.key;
+    tecla = tecla.toLowerCase();
+    if (wordArray.includes(tecla)) {
+      for (var i = 0; i < wordArray.length; i++) {
+        if (wordArray[i] === tecla) {
+          arrayUnderscore[i] = tecla;
+          var wordInGame = arrayUnderscore.join("");
+          wordInGame = wordInGame.toUpperCase();
+          showWord(wordInGame);
+        }
+      }
+    } else {
+      if (validateKey() === true) {
+        if (keyPressed.includes(tecla) || arrayUnderscore.includes(tecla)) {
+          console.log("Esta tecla ya se apretó");
+        } else {
+          keyPressed.push(tecla);
+          var keyString = keyPressed.join(" ");
+          keyString = keyString.toUpperCase();
+          keyGame.textContent = keyString;
+        }
+      } else {
+        console.log("ingrese un caracter valido");
+      }
+    }
+    gameStatus();
+  });
+}
+
+function gameStatus() {
+  if (arrayUnderscore.includes("_")) {
+    console.log("sigue el juego");
+  } else {
+    alert("ganaste");
+  }
 }
